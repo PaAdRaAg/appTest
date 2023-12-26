ff6fcc52f02ab07274f3f9cad88a9023b8024371
PaAdRaAg <pramos12@ucol.mx>
2023-12-26 00:44:24 -0600

    implementación de appNavigator con opciones de home, más y otros

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './components/AppNavigator';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
