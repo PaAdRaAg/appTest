eb0f01890e43b71d3c54e326790ea31656185a49
PaAdRaAg <pramos12@ucol.mx>
2023-12-26 01:25:37 -0600

    desactivando headers de appnavigator

--- components/AppNavigator.tsx
+++ components/AppNavigator.tsx
@@ -10,9 +10,9 @@ const Tab = createBottomTabNavigator();
 const AppNavigator: React.FC = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Muchos" component={MuchosScreen} />
-      <Tab.Screen name="Otros" component={OtrosScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Muchos" component={MuchosScreen} options={{ headerShown: false }}  />
+      <Tab.Screen name="Otros" component={OtrosScreen} options={{ headerShown: false }}  />
     </Tab.Navigator>
   );
 };
