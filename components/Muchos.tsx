65d085ab8bdc3fbe6ad39ddcddf24f34e9727c9a
PaAdRaAg <pramos12@ucol.mx>
2023-12-26 00:47:18 -0600

    componentes para la visualización del respectivo navigator seleccionado

---
+++ components/Muchos.tsx
@@ -0,0 +1,13 @@
+// screens/MuchosScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MuchosScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Muchos Screen</Text>
+    </View>
+  );
+};
+
+export default MuchosScreen;
---
+++ components/Otros.tsx
@@ -0,0 +1,13 @@
+// screens/OtrosScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OtrosScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Otros Screen</Text>
+    </View>
+  );
+};
+
+export default OtrosScreen;
